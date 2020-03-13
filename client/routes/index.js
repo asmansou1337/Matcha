9d04a3c2706ed395369fffb5881def34a4e19954
asmansou <asmae.mansouri92@gmail.com>
2020-03-13 21:22:26 +0100

    update

--- client/routes/index.js
+++ client/routes/index.js
@@ -11,4 +11,14 @@ router.get('/signup', function(req, res, next) {
   res.render('signup');
 });
 
+/* GET Login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET Reset Email page. */
+router.get('/resetEmail', function(req, res, next) {
+  res.render('resetEmail');
+});
+
 module.exports = router;
